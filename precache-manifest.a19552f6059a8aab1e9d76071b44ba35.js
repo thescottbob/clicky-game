af7748fe54d5df8c750f38db90bf4246aaf70cb8
thescottbob <scottmcmains@gmail.com>
2019-01-10 23:45:20 -0800

    Updates

---
+++ precache-manifest.a19552f6059a8aab1e9d76071b44ba35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "15e425af3d6ddee6f046",
+    "url": "/clicky-game/static/js/main.15e425af.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "15e425af3d6ddee6f046",
+    "url": "/clicky-game/static/css/main.690fa4a8.chunk.css"
+  },
+  {
+    "revision": "e208195ff8d00ca1a64564cb1d1d576d",
+    "url": "/clicky-game/index.html"
+  }
+];
